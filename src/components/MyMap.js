ecdfd1dbd27409b3c4658e957f8dee3b17dfc4f2
Unknown <alain.cadenat@depannum.com>
2018-05-18 13:15:59 +0200

    feat: arial overlay added

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,17 +1,20 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import L from 'leaflet';
-
+/* eslint-enable */
 import '../App.css';
-import '../styles/app.css'
+import '../styles/app.css';
 
 class MyMap extends Component {
 
   componentDidMount() {
     const center = {lat: 48.589172, lng: 2.246237};
-    const zoom = 11;
-    const archipossible = {title: 'Archipossble', position: {lat: 48.589472, lng: 2.248539}, type: 'general'}
+    const zoom = 25;
+    const archipossible = {title: 'Archipossble', position: {lat: 48.589472, lng: 2.248539}, type: 'general'};
 
-    // create map
+    /**
+     *  create the map
+     */
     this.map = L.map('map', {
       center: center,
       zoom: zoom,
@@ -22,6 +25,18 @@ class MyMap extends Component {
       ]
     });
 
+    this.poi = new L.LayerGroup();
+    L.marker([48.589172, 2.5]).bindPopup('<b>test archi</b>').addTo(this.map)
+
+    this.baselayers = {
+      imagery : L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
+      ESRI: L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}')};
+    this.baselayers.ESRI.addTo(this.map);
+
+    L.control.layers(this.baselayers, {POI:this.poi}).addTo(this.map);
+    /**
+     * Add Archipossible marker
+     */
     this.archipossibleMarker = L.marker(archipossible.position).addTo(this.map).bindPopup(archipossible.title);
   }
 
